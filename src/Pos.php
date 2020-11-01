mewebstudio/pos

commit 4831b0fd640eaaf597cdb7d11a67dc190629ccdb

PHPUnit more tests

src/Pos.php
@@ -157,6 +157,41 @@ class Pos
     }
 
     /**
+     * @return array
+     */
+    public function getConfig(){
+        return $this->bank->getConfig();
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAccount(){
+        return $this->bank->getAccount();
+    }
+
+    /**
+     * @return array
+     */
+    public function getCurrencies(){
+        return $this->bank->getCurrencies();
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOrder(){
+        return $this->bank->getOrder();
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCard(){
+        return $this->bank->getCard();
+    }
+
+    /**
      * Get 3d Form Data
      *
      * @return array

src/GarantiPos.php
@@ -6,7 +6,6 @@ use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
 use Mews\Pos\Exceptions\UnsupportedPaymentModelException;
 use Mews\Pos\Exceptions\UnsupportedTransactionTypeException;
-use SimpleXMLElement;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
@@ -257,7 +256,7 @@ class GarantiPos implements PosInterface
      * @param double $amount
      * @return int
      */
-    protected function amountFormat($amount)
+    public function amountFormat($amount)
     {
         return (int) str_replace('.', '', number_format($amount, 2, '.', ''));
     }
@@ -719,9 +718,7 @@ class GarantiPos implements PosInterface
             'body'  => $contents
         ]);
 
-        $xml = new SimpleXMLElement($response->getBody());
-
-        $this->data = (object) json_decode(json_encode($xml));
+        $this->data = $this->XMLStringToObject($response->getBody());
 
         return $this;
     }
@@ -1010,5 +1007,45 @@ class GarantiPos implements PosInterface
     public function history(array $meta)
     {
         return $this->statusOrHistory($meta, 'orderhistoryinq');
+    }
+
+    /**
+     * @return array
+     */
+    public function getConfig()
+    {
+        return $this->config;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAccount()
+    {
+        return $this->account;
+    }
+
+    /**
+     * @return array
+     */
+    public function getCurrencies()
+    {
+        return $this->currencies;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOrder()
+    {
+        return $this->order;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCard()
+    {
+        return $this->card;
     }
 }

src/PosHelpersTrait.php
@@ -3,6 +3,7 @@
 namespace Mews\Pos;
 
 use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use SimpleXMLElement;
 
 /**
  * Trait PosHelpersTrait
@@ -37,7 +38,7 @@ trait PosHelpersTrait
         $encoder = new XmlEncoder($rootNodeName);
 
         $xml = $encoder->encode($nodes[$rootNodeName], 'xml', [
-            'xml_encoding'  => $encoding
+            'xml_encoding' => $encoding
         ]);
 
         return $xml;
@@ -51,11 +52,11 @@ trait PosHelpersTrait
      */
     public function printData($data)
     {
-        if ((is_object($data) || is_array($data)) && !count((array) $data)) {
+        if ((is_object($data) || is_array($data)) && !count((array)$data)) {
             $data = null;
         }
 
-        return (string) $data;
+        return (string)$data;
     }
 
     /**
@@ -81,5 +82,18 @@ trait PosHelpersTrait
     public function isError()
     {
         return !$this->isSuccess();
+    }
+
+    /**
+     * Converts XML string to object
+     *
+     * @param string data
+     * @return object
+     */
+    public function XMLStringToObject(string $data)
+    {
+        $xml = new SimpleXMLElement($data);
+
+        return (object)json_decode(json_encode($xml));
     }
 }

src/PosNet.php
@@ -6,7 +6,6 @@ use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
 use Mews\Pos\Exceptions\UnsupportedPaymentModelException;
 use Mews\Pos\Exceptions\UnsupportedTransactionTypeException;
-use SimpleXMLElement;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
@@ -645,9 +644,7 @@ class PosNet implements PosInterface
             'body'      => "xmldata=" . $contents,
         ]);
 
-        $xml = new SimpleXMLElement($response->getBody());
-
-        $this->data = (object) json_decode(json_encode($xml));
+        $this->data = $this->XMLStringToObject($response->getBody());
 
         return $this;
     }
@@ -996,5 +993,40 @@ class PosNet implements PosInterface
     public function history(array $meta)
     {
         return $this->status($meta, true);
+    }
+
+    /**
+     * @return array
+     */
+    public function getConfig(){
+        return $this->config;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAccount(){
+        return $this->account;
+    }
+
+    /**
+     * @return array
+     */
+    public function getCurrencies(){
+        return $this->currencies;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOrder(){
+        return $this->order;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCard(){
+        return $this->card;
     }
 }

tests/EstPostTest.php
@@ -1,0 +1,124 @@
+<?php
+
+namespace Mews\Pos\Tests;
+
+use Mews\Pos\EstPos;
+use PHPUnit\Framework\TestCase;
+
+
+class EstPostTest extends TestCase
+{
+    private $account;
+    private $estpos;
+    private $config;
+    private $card;
+    private $order;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->config = require __DIR__ . '/../config/pos.php';
+
+        $this->account = (object)[
+            'bank' => 'akbank',
+            'model' => '3d',
+            'client_id' => 'XXXXXXX',
+            'username' => 'XXXXXXX',
+            'password' => 'XXXXXXX',
+            'store_key' => 'XXXXXXX',
+            'env' => 'test',
+        ];
+
+        $this->card = (object)[
+            'number' => '5555444433332222',
+            'year' => '21',
+            'month' => '12',
+            'cvv' => '122',
+            'name' => 'ahmet',
+            'type' => 'visa'
+        ];
+
+        $this->order = (object)[
+            'id' => 'order222',
+            'name' => 'siparis veren',
+            'email' => 'test@test.com',
+            'amount' => '100.25',
+            'installment' => 0,
+            'currency' => 'TRY',
+            'success_url' => 'https://domain.com/success',
+            'fail_url' => 'https://domain.com/fail_url',
+            'lang' => 'tr',
+            'rand' => microtime()
+        ];
+
+        $this->estpos = new EstPos(
+            $this->config['banks'][$this->account->bank],
+            $this->account,
+            $this->config['currencies']);
+    }
+
+    public function testInit()
+    {
+        $this->assertEquals($this->config['banks'][$this->account->bank], $this->estpos->getConfig());
+        $this->assertEquals($this->account, $this->estpos->getAccount());
+        $this->assertEquals($this->config['currencies'], $this->estpos->getCurrencies());
+    }
+
+    public function testPrepare()
+    {
+
+        $this->estpos->prepare($this->order, $this->card);
+        $this->assertEquals($this->card, $this->estpos->getCard());
+        $this->assertEquals($this->order, $this->estpos->getOrder());
+    }
+
+    public function testGetCardCode(){
+        $card = $this->card;
+
+        $card->type = '1';
+        $this->estpos->prepare($this->order, $card);
+        $this->assertEquals($card->type, $this->estpos->getCardCode());
+
+        $card->type = 'visa';
+        $this->estpos->prepare($this->order, $card);
+        $this->assertNotNull($this->estpos->getCardCode());
+
+        $card->type = 'master';
+        $this->estpos->prepare($this->order, $card);
+        $this->assertNotNull($this->estpos->getCardCode());
+    }
+
+    public function testGet3DFormData()
+    {
+        $this->estpos->prepare($this->order, $this->card);
+
+        $form = [
+            'gateway' => $this->config['banks'][$this->account->bank]['urls']['gateway'][$this->account->env],
+            'success_url' => $this->order->success_url,
+            'fail_url' => $this->order->fail_url,
+            'rand' => $this->order->rand,
+            'hash' => $this->estpos->create3DHash(),
+            'inputs' => [
+                'clientid'                          => $this->account->client_id,
+                'storetype'                         => $this->account->model,
+                'hash'                              => $this->estpos->create3DHash(),
+                'cardType'                          => $this->estpos->getCardCode(),
+                'pan'                               => $this->card->number,
+                'Ecom_Payment_Card_ExpDate_Month'   => $this->card->month,
+                'Ecom_Payment_Card_ExpDate_Year'    => $this->card->year,
+                'cv2'                               => $this->card->cvv,
+                'firmaadi'                          => $this->order->name,
+                'Email'                             => $this->order->email,
+                'amount'                            => $this->order->amount,
+                'oid'                               => $this->order->id,
+                'okUrl'                             => $this->order->success_url,
+                'failUrl'                           => $this->order->fail_url,
+                'rnd'                               => $this->order->rand,
+                'lang'                              => $this->order->lang,
+                'currency'                          => $this->order->currency,
+            ]
+        ];
+        $this->assertEquals($form, $this->estpos->get3DFormData());
+    }
+}

tests/GarantiPosTest.php
@@ -1,0 +1,98 @@
+<?php
+
+namespace Mews\Pos\Tests;
+
+use Mews\Pos\GarantiPos;
+use PHPUnit\Framework\TestCase;
+
+
+class GarantiPosTest extends TestCase
+{
+    private $account;
+    private $garantiPos;
+    private $config;
+    private $card;
+    private $order;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->config = require __DIR__ . '/../config/pos.php';
+
+        $this->account = (object)[
+            'bank' => 'akbank',
+            'model' => '3d',
+            'client_id' => 'XXXXXXX',
+            'terminal_id' => '13456',
+            'username' => 'XXXXXXX',
+            'password' => 'XXXXXXX',
+            'store_key' => 'XXXXXXX',
+            'env' => 'test',
+        ];
+
+        $this->card = (object)[
+            'number' => '5555444433332222',
+            'year' => '21',
+            'month' => '12',
+            'cvv' => '122',
+            'name' => 'ahmet',
+            'type' => 'visa'
+        ];
+
+        $this->order = (object)[
+            'id' => 'order222',
+            'name' => 'siparis veren',
+            'email' => 'test@test.com',
+            'amount' => '100.25',
+            'installment' => 0,
+            'currency' => 'TRY',
+            'success_url' => 'https://domain.com/success',
+            'fail_url' => 'https://domain.com/fail_url',
+            'lang' => 'tr',
+            'rand' => microtime(),
+            'ip' => '156.155.154.153'
+        ];
+
+        $this->garantiPos = new GarantiPos(
+            $this->config['banks'][$this->account->bank],
+            $this->account,
+            $this->config['currencies']);
+    }
+
+    public function testInit()
+    {
+        $this->assertEquals($this->config['banks'][$this->account->bank], $this->garantiPos->getConfig());
+        $this->assertEquals($this->account, $this->garantiPos->getAccount());
+        $this->assertEquals($this->config['currencies'], $this->garantiPos->getCurrencies());
+    }
+
+    public function testPrepare()
+    {
+
+        $this->garantiPos->prepare($this->order, $this->card);
+        $this->assertEquals($this->card, $this->garantiPos->getCard());
+        $this->assertEquals($this->order, $this->garantiPos->getOrder());
+    }
+
+    public function testGet3DFormData()
+    {
+        $this->garantiPos->prepare($this->order, $this->card);
+
+        $form = [
+            'gateway' => $this->config['banks'][$this->account->bank]['urls']['gateway'][$this->account->env],
+            'success_url' => $this->order->success_url,
+            'fail_url' => $this->order->fail_url,
+            'rand' => $this->order->rand
+        ];
+        $actualForm = $this->garantiPos->get3DFormData();
+        $this->assertNotEmpty($actualForm['inputs']);
+        $this->assertNotEmpty($actualForm['hash']);
+
+        unset($actualForm['inputs']);
+        unset($actualForm['hash']);
+        $this->assertEquals($form, $actualForm);
+    }
+
+
+}

tests/PosNetCryptTest.php
@@ -8,7 +8,7 @@ use PHPUnit\Framework\TestCase;
 
 class PosNetCryptTest extends TestCase
 {
-    protected $crypt;
+    private $crypt;
 
     protected function setUp(): void
     {
@@ -18,8 +18,15 @@ class PosNetCryptTest extends TestCase
 
     public function testDecrypt(){
         $data = '9ACF38C842B3522415364850EAD1909BD43FD590BE3CBD539AD5FF6C7465973ABD61E8371E03282605ED06C994DF394244B7E7DAD54A046510484FAA724330C4C95A527D7891151E7C195D4136CBD70A87D1BD1F75473CF6B45A3F2FA8231DD71FFB4150E0BF4B133ECAA5ACC82CFD74903E21BC6EECB4B33AF39B8AF0C183A64002CFC125A55685C69A13192F3A9A4FDAC860E90C3FB6D125285E9E687BEFBE05707E131FC7ABE25FE35AB114FAE8A247B8C0F3DBA8AA74396D10564B7A0617EED913ED';
+
         $key = '10,10,10,10,10,10,10,10';
         $expected_output = '6706598320;67005551;100;00;YKB_TST_090519001330;0;0;https://setmpos.ykb.com/PosnetWebService/YKBTransactionService;posnettest.ykb.com;2225;N;0;Not authenticated;1557398383820;TL';
+
+        $dc = $this->crypt->decrypt($data, $key);
+        $this->assertEquals($expected_output, $dc);
+
+        $data = '1974BC4B9984FF173F9DF305090E44241243CCC9648349C0D607AECACCB55C1F1ED47452B3AD90785F9BCC6AC7E65450D4E72F31B9FC8F9F55A7D109C2BE966C6DD3F3DE12B3457FF0C6FA8BCDBB4B8E5341C1C3DA327992C28354EB3B5C62472C06B8FB6DF34E351206D2CCD5E323FA26EC3EFF2C25656C74C836954F193E634AF391761F88B5D53DBE1FC61A89DB713DFC983D9605D080ACA857196DD3E7B3C52BFCC914CE47961D76B590ECB34B28113A6E4FAA572958D836B09546A9A62D24F829FE18628DF84504FB02';
+        $expected_output = '6706598320;67825768;100;00;00000000000000000892;0;0;https://setmpos.ykb.com/PosnetWebService/YKBTransactionService;posnettest.ykb.com;2225;N;9;None 3D - Secure Transaction;1586175747626;TL';
 
         $dc = $this->crypt->decrypt($data, $key);
         $this->assertEquals($expected_output, $dc);

commit 3a31864e1747163b7764de6865bacaaa63d285e6

fix problem caused by commit 4831b0f

src/Pos.php
@@ -131,7 +131,7 @@ class Pos
      *
      * @return mixed
      */
-    public function payment($card)
+    public function payment($card = null)
     {
         $this->card = $card;
 