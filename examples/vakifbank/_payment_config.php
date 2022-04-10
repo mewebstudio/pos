871f6c0e8d63539516a72c3a078b2b6a0a2c3fc1
Nuryagdy Mustapayev <ce.nuryagdy@gmail.com>
2022-04-10 15:59:21 +0200

    examples - deleted some duplicated code

--- examples/vakifbank/_payment_config.php
+++ examples/vakifbank/_payment_config.php
@@ -2,31 +2,12 @@
 
 require __DIR__.'/../_main_config.php';
 
-$request = \Symfony\Component\HttpFoundation\Request::createFromGlobals();
-$ip = $request->getClientIp();
-
 $installments = [
     0  => 'Peşin',
     2  => '2 Taksit',
     6  => '6 Taksit',
     12 => '12 Taksit',
 ];
-
-function getGateway(\Mews\Pos\Entity\Account\AbstractPosAccount $account): ?\Mews\Pos\PosInterface
-{
-    try {
-        $pos = \Mews\Pos\Factory\PosFactory::createPosGateway($account);
-        $pos->setTestMode(true);
-
-        return $pos;
-    } catch (\Mews\Pos\Exceptions\BankNotFoundException $e) {
-        dump($e->getCode(), $e->getMessage());
-    } catch (\Mews\Pos\Exceptions\BankClassNullException $e) {
-        dump($e->getCode(), $e->getMessage());
-    }
-
-    return null;
-}
 
 function getNewOrder(
     string $baseUrl,
