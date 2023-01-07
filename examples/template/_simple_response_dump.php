cca3502e27e342f14fc1cce4303dfffc2093abb0
Nuryagdy Mustapayev <ce.nuryagdy@gmail.com>
2023-01-07 18:35:43 +0100

    examples - added more buttons

--- examples/template/_simple_response_dump.php
+++ examples/template/_simple_response_dump.php
@@ -16,6 +16,15 @@
     </dl>
     <hr>
     <div class="text-right">
+        <?php if ($pos->isSuccess()) : ?>
+            <?php if (\Mews\Pos\Gateways\AbstractGateway::TX_STATUS === $transaction) : ?>
+                <a href="<?= $bankTestsUrl ?>/regular/cancel.php" class="btn btn-lg btn-danger">Cancel payment</a>
+                <a href="<?= $bankTestsUrl ?>/regular/refund.php" class="btn btn-lg btn-danger">Refund payment</a>
+            <?php endif; ?>
+            <?php if (\Mews\Pos\Gateways\AbstractGateway::TX_CANCEL === $transaction) : ?>
+                <a href="<?= $bankTestsUrl ?>/regular/status.php" class="btn btn-lg btn-danger">Payment Status</a>
+            <?php endif; ?>
+        <?php endif; ?>
         <a href="index.php" class="btn btn-lg btn-info">&lt; Click to payment form</a>
     </div>
 </div>
