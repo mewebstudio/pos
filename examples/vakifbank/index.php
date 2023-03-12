27766aae387ba1053fbab8e006fb16360a5c1ae9
Nuryagdy Mustapayev <ce.nuryagdy@gmail.com>
2023-03-12 20:27:47 +0100

    examples - naming changes

--- examples/vakifbank/index.php
+++ examples/vakifbank/index.php
@@ -1,6 +1,6 @@
 <?php
 
 require './_payment_config.php';
-$templateTitle = 'VPOS (VakifBank bank)';
+$templateTitle = 'VPOS 7/24 (VakifBank)';
 require '../_templates/_header.php';
 require '../_templates/_footer.php';
