9827d1bd180180064f132cf3a7225c5d09b264af
Nuryagdy Mustapayev <ce.nuryagdy@gmail.com>
2023-03-12 20:53:04 +0100

    update rector rules

--- rector.php
+++ rector.php
@@ -3,6 +3,8 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
@@ -12,35 +14,26 @@ return static function (RectorConfig $rectorConfig): void {
     ]);
 
     $rectorConfig->sets([
-        \Rector\Set\ValueObject\SetList::CODE_QUALITY,
-        \Rector\Set\ValueObject\SetList::CODING_STYLE,
-        \Rector\Set\ValueObject\SetList::EARLY_RETURN,
-        \Rector\Set\ValueObject\SetList::PSR_4,
-        \Rector\Set\ValueObject\LevelSetList::UP_TO_PHP_72,
+        SetList::CODE_QUALITY,
+        SetList::CODING_STYLE,
+        SetList::EARLY_RETURN,
+        SetList::DEAD_CODE,
+        SetList::PSR_4,
+        LevelSetList::UP_TO_PHP_72,
     ]);
 
     $rectorConfig->rules([
         \Rector\CodingStyle\Rector\MethodCall\PreferThisOrSelfMethodCallRector::class,
         \Rector\CodingStyle\Rector\ClassMethod\DataProviderArrayItemsNewlinedRector::class,
-
-        // DEAD_CODE rules
-        \Rector\DeadCode\Rector\Cast\RecastingRemovalRector::class,
-        \Rector\DeadCode\Rector\BooleanAnd\RemoveAndTrueRector::class,
-        \Rector\DeadCode\Rector\Concat\RemoveConcatAutocastRector::class,
-        \Rector\DeadCode\Rector\Return_\RemoveDeadConditionAboveReturnRector::class,
-        \Rector\DeadCode\Rector\Assign\RemoveDoubleAssignRector::class,
-        \Rector\DeadCode\Rector\Array_\RemoveDuplicatedArrayKeyRector::class,
-        \Rector\DeadCode\Rector\FunctionLike\RemoveDuplicatedIfReturnRector::class,
-        \Rector\DeadCode\Rector\StaticCall\RemoveParentCallWithoutParentRector::class,
-        \Rector\DeadCode\Rector\Stmt\RemoveUnreachableStatementRector::class,
-        \Rector\DeadCode\Rector\Foreach_\RemoveUnusedForeachKeyRector::class,
-        \Rector\DeadCode\Rector\If_\RemoveAlwaysTrueIfConditionRector::class,
-        \Rector\DeadCode\Rector\If_\RemoveUnusedNonEmptyArrayBeforeForeachRector::class,
-        \Rector\DeadCode\Rector\If_\SimplifyIfElseWithSameContentRector::class,
-        \Rector\DeadCode\Rector\Ternary\TernaryToBooleanOrFalseToBooleanAndRector::class,
-        \Rector\DeadCode\Rector\Property\RemoveUselessVarTagRector::class,
-
         \Rector\TypeDeclaration\Rector\ClassMethod\ArrayShapeFromConstantArrayReturnRector::class,
+    ]);
+
+    $rectorConfig->skip([
+        \Rector\DeadCode\Rector\ClassMethod\RemoveUselessParamTagRector::class,
+        \Rector\DeadCode\Rector\ClassMethod\RemoveUselessReturnTagRector::class,
+        \Rector\TypeDeclaration\Rector\ClassMethod\ArrayShapeFromConstantArrayReturnRector::class => [
+            __DIR__ . '/src/DataMapper/ResponseDataMapper',
+        ],
     ]);
 
     $rectorConfig->ruleWithConfiguration(\Rector\CodingStyle\Rector\FuncCall\ConsistentPregDelimiterRector::class, [
