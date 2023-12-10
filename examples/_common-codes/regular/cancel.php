68b05d46769af936e31f866199c3be143ae0854e
Nuryagdy Mustapayev <ce.nuryagdy@gmail.com>
2023-12-10 12:29:34 +0100

    examples - remove left over code

--- examples/_common-codes/regular/cancel.php
+++ examples/_common-codes/regular/cancel.php
@@ -55,34 +55,6 @@ function createCancelOrder(PosInterface $pos, \Symfony\Component\HttpFoundation\
     }
 
     return $cancelOrder;
-
-    $refundOrder = [
-        'id'          => $lastResponse['order_id'], // MerchantOrderId
-        'amount'      => $lastResponse['amount'],
-        'currency'    => $lastResponse['currency'] ?? PosInterface::CURRENCY_TRY,
-        'ref_ret_num' => $lastResponse['ref_ret_num'],
-        'ip'          => filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4) ? $ip : '127.0.0.1',
-    ];
-
-    if (get_class($pos) === \Mews\Pos\Gateways\GarantiPos::class) {
-        $refundOrder['email']       = '';
-    } elseif (get_class($pos) === \Mews\Pos\Gateways\KuveytPos::class) {
-        $refundOrder['remote_order_id'] = $lastResponse['remote_order_id'];
-        $refundOrder['auth_code']       = $lastResponse['auth_code'];
-        $refundOrder['trans_id']        = $lastResponse['trans_id'];
-    } elseif (get_class($pos) === \Mews\Pos\Gateways\PayFlexV4Pos::class || get_class($pos) === \Mews\Pos\Gateways\PayFlexCPV4Pos::class) {
-        // çalışmazsa $lastResponse['all']['ReferenceTransactionId']; ile denenmesi gerekiyor.
-        $refundOrder['trans_id'] = $lastResponse['trans_id'];
-    } elseif (get_class($pos) === \Mews\Pos\Gateways\PosNetV1Pos::class || get_class($pos) === \Mews\Pos\Gateways\PosNet::class) {
-        /**
-         * payment_model:
-         * siparis olusturulurken kullanilan odeme modeli
-         * orderId'yi dogru şekilde formatlamak icin zorunlu.
-         */
-        $refundOrder['payment_model'] = $lastResponse['payment_model'] ?? PosInterface::MODEL_3D_SECURE;
-    }
-
-    return $refundOrder;
 }
 
 
