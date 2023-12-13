ab20175265ef9459ac9bfabb7c7e42ce2c45cd2c
Fikret ÇİN <fikret@gurmesoft.com>
2023-12-13 23:20:03 +0300

    refreshtime to refreshTime

--- src/DataMapper/EstPosRequestDataMapper.php
+++ src/DataMapper/EstPosRequestDataMapper.php
@@ -271,7 +271,7 @@ class EstPosRequestDataMapper extends AbstractRequestDataMapperCrypt
             'failUrl'   => $order->fail_url,
             // musteri 3D onay sayfasinda islemleri tamamlamadan kapatirsa, banka bu sayfaya bir istek gonderecek.
             'callbackUrl' => $order->fail_url,
-            'refreshtime' => '10', //callbackUrl sisteminin doğru çalışması için eklenmesi gereken parametre
+            'refreshTime' => '10', //callbackUrl sisteminin doğru çalışması için eklenmesi gereken parametre
             'rnd'       => $order->rand,
             'lang'      => $this->getLang($account, $order),
             'currency'  => $this->mapCurrency($order->currency),
